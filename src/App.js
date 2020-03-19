b2d9a05f5922e302bdd6f6c469124bee0bec6f37
Lauren C <48397414+laurloves2code@users.noreply.github.com>
2020-03-19 10:52:54 -0400

    Adding Reactjs Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container App">
+      <main className="main">
+        <h1 className="display-3 mt-5">Daily Twelve</h1>
+        <p className="lead">Coming Soon! What you do in the first few minutes of the day can help your day be great!</p>
+      </main>
     </div>
   );
 }
