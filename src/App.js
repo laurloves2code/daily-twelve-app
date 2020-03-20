d4e04bf49d528ca88f5baa55f0d5448e610753d6
Lauren C <48397414+laurloves2code@users.noreply.github.com>
2020-03-20 16:20:39 -0400

    added weather component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Weather from './Components/weather.js';
 
 function App() {
   return (
@@ -9,7 +10,10 @@ function App() {
         <h1 className="display-3 mt-5">Daily Twelve</h1>
         <p className="lead">Coming Soon! What you do in the first few minutes of the day can help your day be great!</p>
       </main>
+      <Weather />
     </div>
+
+    
   );
 }
 
---
+++ src/Components/weather.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const weather = () => {
+    return (
+        <div>
+           <h1>Weather Here</h1> 
+        </div>
+    );
+}
+
+export default weather;
