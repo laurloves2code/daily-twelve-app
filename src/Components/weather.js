fe024d35822e20f2e27950256e514198c3c93330
Lauren C <48397414+laurloves2code@users.noreply.github.com>
2020-03-20 21:52:55 -0400

    added weather sunshine image

--- src/Components/weather.js
+++ src/Components/weather.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import '../App.css';
+import sunshine from '../img/sunshine.png';
 
 const weather = () => {
-    return (
+    return ( 
         <div>
-           <h1>Weather Here</h1> 
+           <h1 className="py-4">Louisville</h1>
+           <img src={sunshine} alt="Sunsine"/>
+           <h2 className="pt-4">72&deg;</h2>
         </div>
     );
 }
