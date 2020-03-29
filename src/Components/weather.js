bba25b291edbdd272340446650506908bbcb7de7
Lauren C <48397414+laurloves2code@users.noreply.github.com>
2020-03-28 23:14:27 -0400

    state location and temp rendered in app

--- src/Components/weather.js
+++ src/Components/weather.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 import sunshine from '../img/sunshine.png';
+//import '../Components/enterapp.js';
 
 class Weather extends Component {
 
@@ -8,7 +9,7 @@ class Weather extends Component {
         location: '',
         photoBkg: '',
         skyicon: '',
-        temp: ''
+        tempnow: ''
     };
 
     componentDidMount() {
@@ -19,21 +20,27 @@ class Weather extends Component {
     // testing api - will save apikey in another hidden document
 
     apiWeather = async (
-        url = "http://api.openweathermap.org/data/2.5/weather?q=Louisville&appid={apikey}"
-    ) => {
+        url = `http://api.openweathermap.org/data/2.5/weather?q=Louisville&appid={enter}`
+    ) => {        
         const results = await fetch(url);
         const info = await results.json();
-        this.setState({location: "Atlanta"}); 
+        this.toFahrenheit(info.main.temp);
+        this.setState({location: info.name});        
         console.log(info);
         console.log(this.state.location);
+    };
+
+    toFahrenheit = (number) => {
+        const tempFar = (number - 273.15) * 9/5 + 32;
+        this.setState({tempnow: parseInt(tempFar)}); 
     };
 
     render () {
         return (
             <div className="weather-bkg">
-            <h1 className="py-4">Louisville</h1>
+            <h1 className="py-4">{this.state.location}</h1>
             <img src={sunshine} alt="Sunsine"/>
-            <h2 className="py-4">72&deg;</h2>
+            <h2 className="py-4">{this.state.tempnow}&deg;</h2>
          </div> 
         );
     }
