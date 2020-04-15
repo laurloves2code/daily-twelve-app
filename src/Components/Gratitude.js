d920131b6ad728cc08636a1fc7d2dcf0c7557a31
Lauren C <48397414+laurloves2code@users.noreply.github.com>
2020-04-15 19:30:18 -0400

    revised thoughts saved to localstorage

--- src/Components/Gratitude.js
+++ src/Components/Gratitude.js
@@ -5,10 +5,12 @@ import '../App.css';
 
 class Gratitude extends Component {
 
-    userThoughts;
+    //userThoughts;
 
     constructor(props) {
         super(props);
+
+    // this.state will be set with the data input by the user from the form #gratitude-message
       
         this.state = {
           savedThoughts: [],  
@@ -19,48 +21,72 @@ class Gratitude extends Component {
         this.handleSave=this.handleSave.bind(this);
 
       }
+
+
+    // local storage methods. Stores the Gratitude entries in local storage
+    // componentDidMount will check for local storage data immediately after compoment is rendered
+
+    componentDidMount() {
+        let thoughtsFromStorage = JSON.parse(localStorage.getItem("thoughts"));
+
+    // if there is data in local storage it will update the state with the data
     
-       
-    async updateThought() {
-        console.log(this.state.gratValue);
-        await this.setState({gratValue: 'This is the new input'});
-        console.log(this.state.gratValue);
-        return this.state.gratValue;
-    };
+      if(thoughtsFromStorage) {
+          this.setState(thoughtsFromStorage);
+      }
+    }
+      
+    componentWillUpdate(gratitudeData, nextThought) {
+        localStorage.setItem("thoughts", JSON.stringify(nextThought));
+      }
+    
+// updateThought() method used in early stages for testing the gratValue state.
+    
+    // async updateThought() {
+    //     console.log(this.state.gratValue);
+    //     await this.setState({gratValue: 'This is the new input'});
+    //     console.log(this.state.gratValue);
+    //     return this.state.gratValue;
+    // };
+
+//handleChange() sets the state with the new input from the user
 
     handleChange(event) {
         //const timeNow = Date();
         this.setState({gratValue: {
             date: Date.now(),
-            message: event.target.value}});
+            message: event.target.value}}
+            );
         }
 
-    handleSave() {
-        //this.setState({gratValue: event.target.value});
-        console.log(this.state.gratValue);       
+//handleSave() saves the current gratValue state when the Save button is clicked
+// this new data is added to thebeginning of an array of gratValue objects - savedThoughts
+
+    handleSave() {      
         
         this.state.savedThoughts.unshift(this.state.gratValue);
 
         console.log(this.state.savedThoughts);
 
+// attempting to display the date in new Gratitude post
+
+        // console.log(this.state.gratValue.date);
+        // let theTime = this.state.gratValue.date;
+        // console.log(theTime.getHours());
+
+// sets the gratValue states back to blank for user
+
         this.setState({gratValue: {
             date: '',
             message: ''}});
-      }
+      }   
     
-      componentDidMount() {
-            this.userThoughts = JSON.parse(localStorage.getItem('thought'));
-            
-            if (localStorage.getItem('thought')) {
-                this.setState ({
-                    savedThoughts: this.state.savedThoughts
-                })
-            }            
-        }
 
-      UNSAFE_componentWillUpdate(thought, nextState) {
-            localStorage.setItem(thought, JSON.stringify(nextState));            
-      }
+// displayList() used to test what will be displayed through savedThoughts state
+
+      displayList () {
+        console.log(this.state.savedThoughts);
+    }
        
 
     render () {   
@@ -72,6 +98,14 @@ class Gratitude extends Component {
                 <form className="form-area">
                     <div className="form-group">
                         <label htmlFor="gratitude-message">Thoughts of Gratitude</label>
+
+            {/* // Renders list of 3 recently saved gratitude thoughts */}
+                    <div>
+                    <p>{this.userThoughts}</p>
+                    </div>
+                        
+
+
                         <textarea 
                             className="form-control" 
                             id="gratitude-message" 
